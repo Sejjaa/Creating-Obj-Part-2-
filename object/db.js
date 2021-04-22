00b25ec3e7c280f15427fcd60c3f0538bc758b86
Sead Sinanovic <57816656+Sejjaa@users.noreply.github.com>
2021-04-22 04:07:35 +0200

    Add files via upload

--- object/db.js
+++ object/db.js
@@ -25,117 +25,33 @@ var AllObj;
 
 texts.then( (res) =>  {
 
-	AllObj=res;
-	setTimeout(Extra,100 );
+	AllObj=res.split('\n');
+	setTimeout(Extra,500 );
 
 })
 
+var word=" ";
+var loopArrey=3;
+var loop=0;
 
-var NewObj=[];
-
-var lat=" ";
 function Extra(){
 	
-	var xx = AllObj.split('');
-	var fake=xx[13];
-	let loop1=xx.length;
-	let loop=loop1-5;
+	for(let i=0;i<AllObj.length;i++){
 
-	for(let i=0;i<xx.length;i++){
-
-		if( xx[i] == fake || xx[i]==" " ){
-			NewObj.push(lat);
-			lat=" ";
+		if(i == loopArrey){
+			word=AllObj[i];
+			db[loop].push(word);
+			loop++;
+			loopArrey+=4;
 		}else{
-			  lat += xx[i] ;
-			  
-		}
-
-		if(loop==5){
-			NewObj.push(lat);
-			lat=" ";
+			word=AllObj[i];
+			db[loop].push(word);
 			
 
 		}
+
 
 	}
 
-	NewObj.push(lat);
-
-	var folow=0;
-	
-
-
-		for(let i=0;i<NewObj.length;i++){
-			
-
-			if( i==1 ){
-				let word = NewObj[folow-1]+NewObj[i];
-				db[0].push(word);
-
-			}if( i==4 ){
-				let word = NewObj[folow-2] + NewObj[folow-1]  + NewObj[i];
-				db[0].push(word);
-			}if( i==6 ){
-				let word = NewObj[folow-1]  + NewObj[i];
-				db[0].push(word);
-			}if( i==7 ){
-				let word = NewObj[i];
-				db[0].push(word);
-			}if( i==9 ){
-				let word = NewObj[folow-1]+NewObj[i];
-				db[1].push(word);
-			}if( i==12 ){
-				let word = NewObj[folow-2] + NewObj[folow-1]  + NewObj[i];
-				db[1].push(word);
-			}if( i==14 ){
-				let word =NewObj[folow-1]  + NewObj[i];
-				db[1].push(word);	
-			}if( i==15 ){
-				let word = NewObj[i];
-				db[1].push(word);	
-			}if( i==17 ){
-				let word = NewObj[folow-1]+NewObj[i];
-				db[2].push(word);	
-			}if( i==20 ){
-				let word = NewObj[folow-2] + NewObj[folow-1]  + NewObj[i];
-				db[2].push(word);	
-			}if( i==22 ){
-				let word = NewObj[folow-1]+NewObj[i];
-				db[2].push(word);		
-			}if( i==23 ){
-				let word =NewObj[i];
-				db[2].push(word);	
-			}if( i==25 ){
-				let word = NewObj[folow-1]+NewObj[i];
-				db[3].push(word);	
-			}if( i==29 ){
-				let word =NewObj[folow-3] + NewObj[folow-2] + NewObj[folow-1]  + NewObj[i];
-				db[3].push(word);	
-			}if( i==31 ){
-				let word =NewObj[folow-1]  + NewObj[i];
-				db[3].push(word);	
-			}if( i==32 ){
-				let word =NewObj[i];
-				db[3].push(word);	
-			}if( i==34 ){
-				let word =NewObj[folow-1]  + NewObj[i];
-				db[4].push(word);	
-			}if( i==38 ){
-				let word =NewObj[folow-3] + NewObj[folow-2] + NewObj[folow-1]  + NewObj[i];
-				db[4].push(word);	
-			}if( i==40 ){
-				let word =NewObj[folow-1]  + NewObj[i];
-				db[4].push(word);	
-			}if( i==41 ){
-				let word =NewObj[i];
-				db[4].push(word);	
-			}
-
-
-			folow++;
-		}
-
-	
 
 }
